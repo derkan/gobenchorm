019095bea74af783287b49922898aa258abb8f70
Erkan Durmus <erkan@eurotel.com.tr>
2018-05-02 16:44:15 +0300

    first commit

---
+++ doc.go
@@ -0,0 +1,18 @@
+/*
+Package gobechorm is a benchmarking suite for popular GO ORM/ODM's.
+
+Tests are simple and especially focused on performance of scanning db rows into structs.
+
+It is developed to be used against PostgreSQL database.
+
+Package handling is done with GO's `dep` tool.
+
+To install related packages run
+
+	dep ensure
+
+Related packages will be in vendor directory.
+*/
+package gobenchorm
+
+
